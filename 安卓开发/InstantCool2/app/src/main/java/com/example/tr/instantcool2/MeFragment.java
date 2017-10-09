5d0e2468e56d2ae0323c626321339f99203e97c7
RuiTang <tassassin@sina.com>
2017-10-09 23:55:49 +0800

    安卓即时通讯软件2.3
    学习一波

---
+++ 安卓开发/InstantCool2/app/src/main/java/com/example/tr/instantcool2/MeFragment.java
@@ -0,0 +1,42 @@
+package com.example.tr.instantcool2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MeFragment extends Fragment {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+
+
+
+
+
+}
